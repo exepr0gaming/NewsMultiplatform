61adb666e2ad9189d9e0890cfa5ae00a2ba1c3db
exepr0gaming <exehomework@gmail.com>
2023-08-09 13:34:42 +0300

    Initial Commit

---
+++ NewsMultiplatform/NewsMultiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsMultiplatformApp.swift
+//  NewsMultiplatform
+//
+//  Created by Andrew Kurdin on 09.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsMultiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
